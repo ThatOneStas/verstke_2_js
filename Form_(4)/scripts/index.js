172f49936d0c77ec9eae35f319cace445a263483
Hiko1234 <138216528+ThatOneStas@users.noreply.github.com>
2023-09-25 19:35:26 +0300

    25.09

--- Form_(4)/scripts/index.js
+++ Form_(4)/scripts/index.js
@@ -1,16 +1,25 @@
-function show_password(TARGET) {
-    console.log(TARGET.type)
-    const PASSWORD = document.getElementById(TARGET);
-    if(PASSWORD.type === "password") {
-        PASSWORD.type = "text";
-    } 
-    else {
-        PASSWORD.type = "password";
-    }
-  }
+function show_password(selector) {
+    const TARGET = document.querySelector(selector)
+    // show
+    TARGET.setAttribute("type", "text")
+    // hide eye icon
+}
 
 const getUserRole = (element)=>{
-    console.log('on')
+    const ACTIVE_ELEMENT = element.querySelector(".active")
+    const DATA = ACTIVE_ELEMENT.getAttribute('data-role')
+    return DATA
+}
+
+const getLastUser = (element) => {
+    const VALUE = element.value
+    if(VALUE.length >= 3){
+        return VALUE
+    }
+    else {
+        alert('Name is too short.')
+        return false
+    }
 }
 
 
@@ -23,6 +32,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
     const CHECKBOX = document.querySelector(".form__input_checkbox")
     const SUMBIT = document.querySelector(".form__input_sumbit")
     const REGISTER_FORM = document.querySelector(".form")
+    const INPUT_LAST_NAME = document.querySelector('.form__input_second_name')
     // switch user role
     LF_BLOCK.addEventListener("click", ()=>{
         LEFT_BLOCK.classList.toggle('active')
@@ -36,6 +46,16 @@ document.addEventListener("DOMContentLoaded", ()=>{
     // submit form
     REGISTER_FORM.addEventListener("submit", (e)=>{
         e.preventDefault()
-        getUserRole(LF_BLOCK)
+        let role = getUserRole(LF_BLOCK)
+        let lastUserName = getLastUser(INPUT_LAST_NAME)
+        if(lastUserName){
+            return 
+        }
+        // create data frame
+        const USER_DATA = {
+            role,
+            lastUserName
+        }
+        console.log('USER DATA', USER_DATA)
     })
 })
